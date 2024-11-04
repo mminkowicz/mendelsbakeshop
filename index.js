b4faba1eaf6b1c088bcf1d43afc17bd4fc5be966
Mendel Minkowicz <mendelminkowicz@Mendels-MBP.attlocal.net>
2024-11-03 21:56:47 -0500

    On the job 1 completed

---
+++ index.js
@@ -0,0 +1,24 @@
+const storeName = "mendelsBakeShop";
+const storeType = "bakery";
+
+const product1 = "Cheesecake";
+const product2 = "Croissant";
+const product3 = "Tarts";
+const product4 = "Coffee";
+const product5 = "Cake";
+
+const price1 = 20.99;
+const price2 = 8.99;
+const price3 = 12.99;
+const price4 = 4.99;
+const price5 = 59.99;
+
+const storeHours = "Monday - Friday: 9 AM - 5 PM, Saturday: Closed, Sunday: 10 AM - 4 PM";
+
+console.log(storeName, storeType);
+console.log(product1, price1);
+console.log(product2, price2);
+console.log(product3, price3);
+console.log(product4, price4);
+console.log(product5, price5);
+console.log(storeHours);
